a205645b2d2b9700c91fd41fec896ad7085a1d72
Moosa Raza <razasyedmoosa882@gmail.com>
2026-01-11 00:18:24 +0500

    Manage jobs page

---
+++ client/src/pages/ManageJobs.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { manageJobsData } from '../assets/assets'
+import moment from 'moment'
+import { useNavigate } from 'react-router-dom'
+
+const ManageJobs = () => {
+    const navigate = useNavigate();
+  return (
+    <div className='container p-4 max-w-5xl'>
+        <div className='overflow-x-auto'>
+            <table className='min-w-full bg-white border border-gray-200 max-sm:text-sm'>
+                <thead>
+                    <tr>
+                        <th className='px-4 py-2 border-b text-left max-sm:hidden'>#</th>
+                        <th className='px-4 py-2 border-b text-left'>Job Title </th>
+                        <th className='px-4 py-2 border-b text-left max-sm:hidden'>Date</th>
+                        <th className='px-4 py-2 border-b text-left max-sm:hidden'>Location</th>
+                        <th className='px-4 py-2 border-b text-left'>Applicants</th>
+                        <th className='px-4 py-2 border-b text-left'>Visible</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {manageJobsData.map((job,index)=>(
+                        <tr key={index} className='text-gray-700'>
+                            <td className='px-4 py-2 border-b max-sm:hidden'>{index+1}</td>
+                            <td className='px-4 py-2 border-b'>{job.title}</td>
+                            <td className='px-4 py-2 border-b max-sm:hidden'>{moment(job.date).format('ll')}</td>
+                            <td className='px-4 py-2 border-b max-sm:hidden'>{job.location}</td>
+                            <td className='px-4 py-2 border-b'>{job.applicants}</td>
+                            <td className='px-4 py-2 border-b'>
+                                <input className='scale-124 ml-4' type='checkbox' name='' id='' />
+                            </td>
+
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+        <div className='mt-4 flex justify-end'>
+            <button onClick={e=>navigate('/dashboard/add-job')} className='bg-black text-white py-2 px-4 rounded'>Add New Job</button>
+        </div>
+    </div>
+  )
+}
+
+export default ManageJobs
